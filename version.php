d57028aeda541444781c77dd8f1df19d1c900a83
Bruno Rodrigues <brunowebr@gmail.com>
2022-09-04 20:18:32 -0300

    Create a static version file

---
+++ version.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * Version
+ * static dependency versions
+ */
+
+// Current project
+const NIMBUS_ICONS_PHP_VERSION = '0.1.0';
+
+/* Nimbus icon dependency (only "/icon/*.svg" dir)
+ * https://github.com/TiendaNube/nimbus-icons
+ */
+const NIMBUS_ICONS_VERSION = '0.2.7';
