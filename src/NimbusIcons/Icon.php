39c52bb22293053c2bc813bb8e8ffc0991b98dd0
Bruno Rodrigues <brunowebr@gmail.com>
2022-09-04 19:30:56 -0300

    Create base class to get Icons

--- src/NimbusIcons/Icon.php
+++ src/NimbusIcons/Icon.php
@@ -1,1 +1,25 @@
 <?php
+
+/*
+ * Nimbus Icons
+ * Method to show Tiendanube Nimbus icons inline
+ * Base repo: https://github.com/TiendaNube/nimbus-icons
+ */
+
+namespace Nimbus;
+
+class Icon {
+    private const NIMBUS_ICONS_PATH = './static/nimbus-icons/';
+
+    public static function get(string $name, int $size = 16, string $color = 'currentColor') {
+        $icon_source = file_get_contents(self::NIMBUS_ICONS_PATH . $name . '.svg');
+
+        $output = strtr($icon_source, array(
+            'width="16"' => 'width="'. ($size) .'"',
+            'height="16"' => 'height="'. ($size) .'"',
+            '<svg' => '<svg fill="'. $color .'"'
+        ));
+
+        return $output;
+    }
+}
