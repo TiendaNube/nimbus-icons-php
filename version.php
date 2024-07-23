fb98d956753b681eb56ee639cea2fdcb90b8e498
Bruno Rodrigues <brodrigues@nuvemshop.com.br>
2024-07-23 10:57:43 -0300

    Upgrade versions

--- version.php
+++ version.php
@@ -6,9 +6,9 @@
  */
 
 // Current project
-const NIMBUS_ICONS_PHP_VERSION = '0.1.1';
+const NIMBUS_ICONS_PHP_VERSION = '0.1.2';
 
 /* Nimbus icon dependency (only "/icon/*.svg" dir)
  * https://github.com/TiendaNube/nimbus-icons
  */
-const NIMBUS_ICONS_VERSION = '0.2.9';
+const NIMBUS_ICONS_VERSION = '1.5.0';
