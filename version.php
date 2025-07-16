c44fcf53e48fbfe5a30996f53b4817c225733fd4
Bruno Rodrigues <brodrigues@nuvemshop.com.br>
2025-07-16 17:23:00 -0300

    feat: add new icons

--- version.php
+++ version.php
@@ -11,4 +11,4 @@ const NIMBUS_ICONS_PHP_VERSION = '0.1.4';
 /* Nimbus icon dependency (only "/src/assets*.svg" dir)
  * https://github.com/TiendaNube/nimbus-design-system/tree/master/packages/icons
  */
-const NIMBUS_ICONS_VERSION = '1.5.0';
+const NIMBUS_ICONS_VERSION = '1.11.0';
